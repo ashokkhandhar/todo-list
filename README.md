439abe483d79ef0c4add782078444141a37f5303
ashok khandhar <ashokkhandhar18@gmail.com>
2024-03-30 16:42:40 +0530

    version 2.0

--- README.md
+++ README.md
@@ -1,40 +1,15 @@
 # Todo List
 
-It's a application of to make todo list.
+It's a real life application for makeing todo list.
 
 ## Technologies
 
 - nodeJS
 - express
+- MongoDB
 - EJS
 - CSS
 
 ## Live
 
 https://todo-list-wr0k.onrender.com/
-
-## Run Locally
-
-To clone the project run following command in terminal. Make sure you have git installed.
-
-```bash
-git clone https://github.com/ashokkhandhar/todo-list.git
-```
-
-Open project directory in terminal and run following command. Make sure you have node installed.
-
-```bash
-npm install
-```
-
-Now start server by running following command.
-
-```bash
-node index.js
-```
-
-Go to browser and paste link given below.
-
-```bash
-http://localhost:3000/
-```
--- index.js
+++ index.js
@@ -1,37 +1,117 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import mongoose from 'mongoose';
+import _ from 'lodash';
+import 'dotenv/config'
 
 const app = express();
 const port = 3000;
 
-let list = new Array();
-let workList = new Array();
+// middlewares
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
 
-app.get('/', (req, res)=>{
-    const today = new Date().toLocaleDateString('en-US', {weekday: "long", day: "numeric", month: "long"});
-    res.render("index.ejs", {listTitle: today, listItems: list});
+// db connections
+
+mongoose.connect(process.env.URL);
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "item must have text content."],
+    },
+});
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema],
 });
 
-app.post('/', (req, res)=>{
-    const newItem = req.body['newItem'];
-    if(req.body.list === "Work"){
-        workList = [newItem, ...workList];
-        res.redirect("/work");
-    } else{
-        list = [newItem, ...list];
-        res.redirect("/");
+const List = mongoose.model("list", listSchema);
+const Item = mongoose.model("item", itemSchema);
+
+app.get("/", async (req, res) => {
+    try {
+        const responce = await List.find();
+        res.render("index.ejs", {lists: responce});
+    } catch (error) {
+        res.send(error.message);
     }
 });
 
-app.get('/work', (req, res)=>{
-    res.render("index.ejs", {listTitle: "Work List", listItems: workList});
+app.post("/newList", async (req, res) => {
+    const newListName = _.capitalize(req.body.listName);
+    try {
+        const responce = await List.findOne({name: newListName});
+        if(responce === null) {
+            const newItem = new List({
+                name: newListName,
+                items: [],
+            });
+            try {
+                const responce = await newItem.save();
+                console.log("Sucessfully created new list..!\n", responce);
+                res.redirect(`/${newListName}`);
+            } catch (error) {
+                res.send(error.message);
+            }
+        }
+        else {
+            res.redirect(`/${newListName}`);
+        }
+    } catch(error) {
+        res.send(error.message);
+    }
+});
+
+app.get("/:listName", async (req, res) => {
+    const listName = req.params.listName;
+    try {
+        const list = await List.find({name: listName});
+        const lists = await List.find();
+        res.render("index.ejs", {lists: lists, list: list[0]});
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.post("/deleteList", async (req, res) => {
+    const listName = req.body.listName;
+    try {
+        const responce = await List.deleteOne({name: listName});
+        console.log(responce);
+        res.redirect("/");
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.post("/newItem", async (req, res) => {
+    const itemName = req.body.itemName;
+    const listName = req.body.listName;
+    const newItem = new Item({
+        name: itemName,
+    });
+    try {
+        const list = await List.findOne({name: listName});
+        list.items.push(newItem);
+        const responce = await list.save();
+        console.log("Sucessfully created new item..!\n", responce);
+        res.redirect(`/${listName}`);
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.post("/deleteItem", async (req, res) => {
+    const listName = req.body.listName;
+    try {
+        const responce = await List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: req.body.itemId}}});
+        console.log(responce);
+        res.redirect(`/${listName}`);
+    } catch (error) {
+        res.send(error.message);
+    }
 });
 
 app.listen(port, ()=>{
